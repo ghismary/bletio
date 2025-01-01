9f5ca0b46666d1827d7b515147f120f5b562dbe8
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2025-01-01 16:17:45 +0100

    refactor: Enhance LeState
    Refactor the LeState enum to include a simplify method for better state comparison.
    Update SingleLeState and CombinedLeState to LeSingleState and LeCombinedState respectively.
    Implement PartialEq for LeState and LeCombinedState, ensuring correct equality checks.
    Add unit tests to validate the new logic and ensure expected behavior for state comparisons.

--- src/le_states.rs
+++ src/le_states.rs
@@ -1,11 +1,40 @@
-#[derive(Debug)]
+use core::cmp::Ordering;
+
+#[derive(Debug, Clone, Copy, Eq)]
 pub enum LeState {
-    Single(SingleLeState),
-    Combined(CombinedLeState),
+    Single(LeSingleState),
+    Combined(LeCombinedState),
 }
 
-#[derive(Debug, Eq, PartialEq)]
-pub enum SingleLeState {
+impl PartialEq for LeState {
+    fn eq(&self, other: &Self) -> bool {
+        let mut first = *self;
+        let mut second = *other;
+        first = first.simplify();
+        second = second.simplify();
+        match (first, second) {
+            (LeState::Single(first), LeState::Single(second)) => first == second,
+            (LeState::Combined(first), LeState::Combined(second)) => first == second,
+            _ => false,
+        }
+    }
+}
+
+impl LeState {
+    pub(crate) fn simplify(self) -> Self {
+        match &self {
+            LeState::Single(_) => self,
+            LeState::Combined(LeCombinedState(first, second)) => match first.cmp(second) {
+                Ordering::Equal => LeState::Single(*first),
+                Ordering::Less => LeState::Combined(LeCombinedState(*first, *second)),
+                Ordering::Greater => LeState::Combined(LeCombinedState(*second, *first)),
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
+pub enum LeSingleState {
     ScannableAdvertising,
     ConnectableAdvertising,
     NonConnectableAdvertising,
@@ -18,13 +47,68 @@ pub enum SingleLeState {
     ConnectionSlaveRole,
 }
 
-#[derive(Debug)]
-pub struct CombinedLeState(pub SingleLeState, pub SingleLeState);
+#[derive(Debug, Clone, Copy, Eq)]
+pub struct LeCombinedState(pub LeSingleState, pub LeSingleState);
 
-impl PartialEq for CombinedLeState {
+impl PartialEq for LeCombinedState {
     fn eq(&self, other: &Self) -> bool {
         ((self.0 == other.0) && (self.1 == other.1)) || ((self.0 == other.1) && (self.1 == other.0))
     }
 }
 
-impl Eq for CombinedLeState {}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_same_combined_states_eq() {
+        assert_eq!(
+            LeState::Combined(LeCombinedState(
+                LeSingleState::NonConnectableAdvertising,
+                LeSingleState::PassiveScanning
+            )),
+            LeState::Combined(LeCombinedState(
+                LeSingleState::NonConnectableAdvertising,
+                LeSingleState::PassiveScanning
+            ))
+        )
+    }
+
+    #[test]
+    fn test_different_combined_states_ne() {
+        assert_ne!(
+            LeState::Combined(LeCombinedState(
+                LeSingleState::NonConnectableAdvertising,
+                LeSingleState::PassiveScanning
+            )),
+            LeState::Combined(LeCombinedState(
+                LeSingleState::ConnectableAdvertising,
+                LeSingleState::ActiveScanning
+            ))
+        )
+    }
+    #[test]
+    fn test_inverted_combined_states_eq() {
+        assert_eq!(
+            LeState::Combined(LeCombinedState(
+                LeSingleState::NonConnectableAdvertising,
+                LeSingleState::PassiveScanning
+            )),
+            LeState::Combined(LeCombinedState(
+                LeSingleState::PassiveScanning,
+                LeSingleState::NonConnectableAdvertising
+            ))
+        )
+    }
+
+    #[test]
+    fn test_equivalent_single_and_combined_states_eq() {
+        assert_eq!(
+            LeState::Single(LeSingleState::ActiveScanning),
+            LeState::Combined(LeCombinedState(
+                LeSingleState::ActiveScanning,
+                LeSingleState::ActiveScanning
+            ))
+        )
+    }
+}
