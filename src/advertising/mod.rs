06e539b070913ec8ec429cd187ea42a08b573451
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2025-01-03 15:57:14 +0100

    feat: Add advertising parameters
    Introduce a new module for advertising parameters, including structures and enums for advertising interval values, types, address types, and channel maps.
    Implement validation and  encoding methods for advertising parameters.

--- src/advertising/mod.rs
+++ src/advertising/mod.rs
@@ -1,8 +1,13 @@
 mod ad_struct;
 mod advertising_data;
+mod advertising_parameters;
 
 pub use ad_struct::service_uuid::{
     ServiceUuid128AdStruct, ServiceUuid16AdStruct, ServiceUuid32AdStruct,
 };
 pub use ad_struct::AdStruct;
 pub use advertising_data::AdvertisingData;
+pub use advertising_parameters::{
+    AdvertisingChannelMap, AdvertisingFilterPolicy, AdvertisingIntervalValue,
+    AdvertisingParameters, AdvertisingType, OwnAddressType, PeerAddress, PeerAddressType,
+};
