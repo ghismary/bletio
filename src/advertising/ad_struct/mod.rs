d698fb1aa3f5862d080ab43471c339b8dcfc30fe
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2025-01-04 23:54:39 +0100

    feat: Add FlagsAdStruct for advertising data
    Introduce FlagsAdStruct to represent advertising flags. Update AdStruct to include FlagsAdStruct and implement encoding logic.
    Add methods to AdvertisingData for setting and checking flags.
    Include bitflags dependency for better flag management.

--- src/advertising/ad_struct/mod.rs
+++ src/advertising/ad_struct/mod.rs
@@ -1,11 +1,14 @@
-pub mod service_uuid;
 mod common_data_types;
+mod flags;
+mod service_uuid;
 
-use service_uuid::{ServiceUuid128AdStruct, ServiceUuid16AdStruct, ServiceUuid32AdStruct};
 use crate::Error;
+pub use flags::FlagsAdStruct;
+pub use service_uuid::{ServiceUuid128AdStruct, ServiceUuid16AdStruct, ServiceUuid32AdStruct};
 
 #[derive(Debug)]
 pub enum AdStruct {
+    Flags(FlagsAdStruct),
     ServiceUuid16(ServiceUuid16AdStruct),
     ServiceUuid32(ServiceUuid32AdStruct),
     ServiceUuid128(ServiceUuid128AdStruct),
@@ -14,6 +17,7 @@ pub enum AdStruct {
 impl AdStruct {
     pub(crate) fn size(&self) -> usize {
         match self {
+            AdStruct::Flags(value) => value.size(),
             AdStruct::ServiceUuid16(value) => value.size(),
             AdStruct::ServiceUuid32(value) => value.size(),
             AdStruct::ServiceUuid128(value) => value.size(),
@@ -22,6 +26,7 @@ impl AdStruct {
 
     pub(crate) fn encode(&self, buffer: &mut [u8]) -> Result<usize, Error> {
         match self {
+            AdStruct::Flags(value) => value.encode(buffer),
             AdStruct::ServiceUuid16(value) => value.encode(buffer),
             AdStruct::ServiceUuid32(value) => value.encode(buffer),
             AdStruct::ServiceUuid128(value) => value.encode(buffer),
--- src/advertising/mod.rs
+++ src/advertising/mod.rs
@@ -2,10 +2,10 @@ mod ad_struct;
 mod advertising_data;
 mod advertising_parameters;
 
-pub use ad_struct::service_uuid::{
-    ServiceUuid128AdStruct, ServiceUuid16AdStruct, ServiceUuid32AdStruct,
-};
 pub use ad_struct::AdStruct;
+pub use ad_struct::{
+    FlagsAdStruct, ServiceUuid128AdStruct, ServiceUuid16AdStruct, ServiceUuid32AdStruct,
+};
 pub use advertising_data::AdvertisingData;
 pub use advertising_parameters::{
     AdvertisingChannelMap, AdvertisingFilterPolicy, AdvertisingIntervalValue,
