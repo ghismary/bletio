acdb78ebef7cc43c313450dbf6c0e0bd73387490
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2025-01-20 21:45:42 +0100

    feat(uri): Check validity of custom URI scheme at compile-time

--- src/advertising/ad_struct/uri.rs
+++ src/advertising/ad_struct/uri.rs
@@ -6,6 +6,7 @@ use crate::Error;
 
 const EMPTY_SCHEME_NAME_VALUE: u16 = 0x0001;
 
+/// An URI to be included in the Universal Resource Identifier Advertising Structure.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Uri {
     scheme: UriScheme,
@@ -13,11 +14,12 @@ pub struct Uri {
 }
 
 impl Uri {
+    /// Create an URI.
     pub fn new(scheme: impl Into<UriScheme>, hier_part: &'static str) -> Self {
-        Self {
-            scheme: scheme.into(),
-            hier_part,
+        fn inner(scheme: UriScheme, hier_part: &'static str) -> Uri {
+            Uri { scheme, hier_part }
         }
+        inner(scheme.into(), hier_part)
     }
 
     fn try_encode(
@@ -29,44 +31,202 @@ impl Uri {
     }
 }
 
+#[diagnostic::on_unimplemented(message = "the URI scheme cannot be empty")]
+#[doc(hidden)]
+pub trait CustomUriSchemeNotEmpty {}
+
+#[doc(hidden)]
+pub struct CustomUriSchemeIsNotEmpty<const VALID: bool>;
+
+#[doc(hidden)]
+impl CustomUriSchemeNotEmpty for CustomUriSchemeIsNotEmpty<true> {}
+
+#[doc(hidden)]
+pub const fn custom_uri_scheme_is_not_empty<T: CustomUriSchemeNotEmpty>() {}
+
+#[doc(hidden)]
+pub const fn check_custom_uri_scheme_is_not_empty(scheme: &str) -> bool {
+    !scheme.is_empty()
+}
+
+#[diagnostic::on_unimplemented(message = "the URI scheme cannot contain non-ascii characters")]
+#[doc(hidden)]
+pub trait CustomUriSchemeOnlyAsciiChars {}
+
+#[doc(hidden)]
+pub struct CustomUriSchemeHasOnlyAsciiChars<const VALID: bool>;
+
+#[doc(hidden)]
+impl CustomUriSchemeOnlyAsciiChars for CustomUriSchemeHasOnlyAsciiChars<true> {}
+
+#[doc(hidden)]
+pub const fn custom_uri_scheme_has_only_ascii_chars<T: CustomUriSchemeOnlyAsciiChars>() {}
+
+#[doc(hidden)]
+pub const fn check_custom_uri_scheme_has_only_ascii_chars(scheme: &str) -> bool {
+    scheme.is_ascii()
+}
+
+#[diagnostic::on_unimplemented(
+    message = "the URI scheme cannot contain characters other than alphanumeric, '-' and '.'"
+)]
+#[doc(hidden)]
+pub trait CustomUriSchemeOnlyValidChars {}
+
+#[doc(hidden)]
+pub struct CustomUriSchemeHasOnlyValidChars<const VALID: bool>;
+
+#[doc(hidden)]
+impl CustomUriSchemeOnlyValidChars for CustomUriSchemeHasOnlyValidChars<true> {}
+
+#[doc(hidden)]
+pub const fn custom_uri_scheme_has_only_valid_chars<T: CustomUriSchemeOnlyValidChars>() {}
+
+#[doc(hidden)]
+pub const fn check_custom_uri_scheme_has_only_valid_chars(scheme: &str) -> bool {
+    let mut i = 0;
+    while i < scheme.as_bytes().len() {
+        let c = scheme.as_bytes()[i] as char;
+        if !(c.is_ascii_alphanumeric() || c == '-' || c == '.') {
+            return false;
+        }
+        i += 1;
+    }
+    true
+}
+
+#[diagnostic::on_unimplemented(
+    message = "the URI scheme cannot start with a non-alphabetic character"
+)]
+#[doc(hidden)]
+pub trait CustomUriSchemeAlphabeticFirstChar {}
+
+#[doc(hidden)]
+pub struct CustomUriSchemeHasAlphabeticFirstChar<const VALID: bool>;
+
+#[doc(hidden)]
+impl CustomUriSchemeAlphabeticFirstChar for CustomUriSchemeHasAlphabeticFirstChar<true> {}
+
+#[doc(hidden)]
+pub const fn custom_uri_scheme_has_alphabetic_first_char<T: CustomUriSchemeAlphabeticFirstChar>() {}
+
+#[doc(hidden)]
+pub const fn check_custom_uri_scheme_has_alphabetic_first_char(scheme: &str) -> bool {
+    if scheme.is_empty() {
+        return true; // Not really true, but the check on emptiness will trigger
+    }
+    let c = scheme.as_bytes()[0];
+    c.is_ascii_alphabetic()
+}
+
+#[diagnostic::on_unimplemented(
+    message = "the URI scheme cannot end with a non-alphanumeric character"
+)]
+#[doc(hidden)]
+pub trait CustomUriSchemeAlphanumericLastChar {}
+
+#[doc(hidden)]
+pub struct CustomUriSchemeHasAlphanumericLastChar<const VALID: bool>;
+
+#[doc(hidden)]
+impl CustomUriSchemeAlphanumericLastChar for CustomUriSchemeHasAlphanumericLastChar<true> {}
+
+#[doc(hidden)]
+pub const fn custom_uri_scheme_has_alphanumeric_last_char<
+    T: CustomUriSchemeAlphanumericLastChar,
+>() {
+}
+
+#[doc(hidden)]
+pub const fn check_custom_uri_scheme_has_alphanumeric_last_char(scheme: &str) -> bool {
+    if scheme.is_empty() {
+        return true; // Not really true, but the check on emptiness will trigger
+    }
+    let c = scheme.as_bytes()[scheme.as_bytes().len() - 1];
+    c.is_ascii_alphanumeric()
+}
+
+/// A custom URI scheme if the scheme you want to use is not defined in [`ProvisionedUriScheme`].
+///
+/// Use [`custom_uri_scheme`] to create it.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct CustomUriScheme {
     scheme: &'static str,
 }
 
 impl CustomUriScheme {
-    pub fn try_new(scheme: &'static str) -> Result<CustomUriScheme, AdvertisingError> {
-        scheme.try_into()
+    #[doc(hidden)]
+    pub const fn new(scheme: &'static str) -> Self {
+        Self { scheme }
     }
 }
 
-impl TryFrom<&'static str> for CustomUriScheme {
-    type Error = AdvertisingError;
-
-    fn try_from(value: &'static str) -> Result<Self, Self::Error> {
-        let contains_invalid_characters = value
-            .chars()
-            .any(|c| !(c.is_ascii_alphanumeric() || c == '-' || c == '.'));
-        let first_character_is_invalid = value
-            .chars()
-            .nth(0)
-            .is_none_or(|c| !c.is_ascii_alphabetic());
-        let last_character_is_invalid = value
-            .chars()
-            .last()
-            .is_none_or(|c| !c.is_ascii_alphanumeric());
-
-        if contains_invalid_characters || first_character_is_invalid || last_character_is_invalid {
-            Err(AdvertisingError::InvalidCustomUriScheme(value))
-        } else {
-            Ok(Self { scheme: value })
-        }
-    }
+/// Create a [`CustomUriScheme`], checking that it is valid at compile-time.
+///
+/// # Examples
+///
+/// ```
+/// # use bletio::advertising::ad_struct::custom_uri_scheme;
+/// let scheme = custom_uri_scheme!("custom");
+/// ```
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __custom_uri_scheme__ {
+    ($scheme:expr) => {{
+        const NOT_EMPTY_ERR: bool =
+            $crate::advertising::ad_struct::uri::check_custom_uri_scheme_is_not_empty($scheme);
+        $crate::advertising::ad_struct::uri::custom_uri_scheme_is_not_empty::<
+            $crate::advertising::ad_struct::uri::CustomUriSchemeIsNotEmpty<NOT_EMPTY_ERR>,
+        >();
+        const ONLY_ASCII_CHARS_ERR: bool =
+            $crate::advertising::ad_struct::uri::check_custom_uri_scheme_has_only_ascii_chars(
+                $scheme,
+            );
+        $crate::advertising::ad_struct::uri::custom_uri_scheme_has_only_ascii_chars::<
+            $crate::advertising::ad_struct::uri::CustomUriSchemeHasOnlyAsciiChars<
+                ONLY_ASCII_CHARS_ERR,
+            >,
+        >();
+        const ONLY_VALID_CHARS_ERR: bool =
+            $crate::advertising::ad_struct::uri::check_custom_uri_scheme_has_only_valid_chars(
+                $scheme,
+            );
+        $crate::advertising::ad_struct::uri::custom_uri_scheme_has_only_valid_chars::<
+            $crate::advertising::ad_struct::uri::CustomUriSchemeHasOnlyValidChars<
+                ONLY_VALID_CHARS_ERR,
+            >,
+        >();
+        const ALPHABETIC_FIRST_CHAR_ERR: bool =
+            $crate::advertising::ad_struct::uri::check_custom_uri_scheme_has_alphabetic_first_char(
+                $scheme,
+            );
+        $crate::advertising::ad_struct::uri::custom_uri_scheme_has_alphabetic_first_char::<
+            $crate::advertising::ad_struct::uri::CustomUriSchemeHasAlphabeticFirstChar<
+                ALPHABETIC_FIRST_CHAR_ERR,
+            >,
+        >();
+        const ALPHANUMERIC_LAST_CHAR_ERR: bool =
+            $crate::advertising::ad_struct::uri::check_custom_uri_scheme_has_alphanumeric_last_char(
+                $scheme,
+            );
+        $crate::advertising::ad_struct::uri::custom_uri_scheme_has_alphanumeric_last_char::<
+            $crate::advertising::ad_struct::uri::CustomUriSchemeHasAlphanumericLastChar<
+                ALPHANUMERIC_LAST_CHAR_ERR,
+            >,
+        >();
+        $crate::advertising::ad_struct::uri::CustomUriScheme::new($scheme)
+    }};
 }
 
+#[doc(inline)]
+pub use __custom_uri_scheme__ as custom_uri_scheme;
+
+/// An URI scheme, either provisioned or custom.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum UriScheme {
+    /// A provisioned URI scheme.
     Provisioned(ProvisionedUriScheme),
+    /// A custom URI scheme.
     Custom(CustomUriScheme),
 }
 
@@ -94,6 +254,12 @@ impl UriScheme {
 impl From<ProvisionedUriScheme> for UriScheme {
     fn from(value: ProvisionedUriScheme) -> Self {
         Self::Provisioned(value)
+    }
+}
+
+impl From<CustomUriScheme> for UriScheme {
+    fn from(value: CustomUriScheme) -> Self {
+        Self::Custom(value)
     }
 }
 
@@ -152,64 +318,186 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_custom_uri_scheme_creation_success() -> Result<(), AdvertisingError> {
-        let scheme = CustomUriScheme::try_new("custom")?;
+    fn test_check_custom_uri_scheme_is_not_empty() {
+        assert!(!check_custom_uri_scheme_is_not_empty(""));
+        assert!(check_custom_uri_scheme_is_not_empty("custom"));
+    }
+
+    #[test]
+    fn test_check_custom_uri_scheme_has_only_ascii_chars() {
+        assert!(check_custom_uri_scheme_has_only_ascii_chars("custom"));
+        assert!(!check_custom_uri_scheme_has_only_ascii_chars("custおm"));
+    }
+
+    #[test]
+    fn test_check_custom_uri_scheme_has_only_valid_chars() {
+        assert!(check_custom_uri_scheme_has_only_valid_chars("custom"));
+        assert!(!check_custom_uri_scheme_has_only_valid_chars("cus/om"));
+    }
+
+    #[test]
+    fn test_check_custom_uri_scheme_has_alphabetic_first_char() {
+        assert!(check_custom_uri_scheme_has_alphabetic_first_char("custom"));
+        assert!(!check_custom_uri_scheme_has_alphabetic_first_char(
+            ".custom"
+        ));
+        assert!(!check_custom_uri_scheme_has_alphabetic_first_char(
+            "2custom"
+        ));
+        assert!(check_custom_uri_scheme_has_alphabetic_first_char(""));
+    }
+
+    #[test]
+    fn test_check_custom_uri_scheme_has_alphanumeric_last_char() {
+        assert!(check_custom_uri_scheme_has_alphanumeric_last_char("custom"));
+        assert!(!check_custom_uri_scheme_has_alphanumeric_last_char(
+            "custom-"
+        ));
+        assert!(check_custom_uri_scheme_has_alphanumeric_last_char(""));
+    }
+
+    #[test]
+    fn test_custom_uri_scheme() {
+        let scheme = custom_uri_scheme!("custom");
         assert_eq!(scheme.scheme, "custom");
 
-        let scheme: CustomUriScheme = "cus-tom".try_into()?;
+        let scheme = custom_uri_scheme!("cus-tom");
         assert_eq!(scheme.scheme, "cus-tom");
 
-        let scheme: CustomUriScheme = "cu.st.om".try_into()?;
+        let scheme = custom_uri_scheme!("cu.st.om");
         assert_eq!(scheme.scheme, "cu.st.om");
 
-        let scheme = CustomUriScheme::try_new("cust0m")?;
+        let scheme = custom_uri_scheme!("cust0m");
         assert_eq!(scheme.scheme, "cust0m");
 
-        let scheme = CustomUriScheme::try_new("be3")?;
+        let scheme = custom_uri_scheme!("be3");
         assert_eq!(scheme.scheme, "be3");
+    }
+
+    #[test]
+    fn test_uri_scheme_try_encode_success() -> Result<(), AdvertisingError> {
+        let mut buffer = AdStructBuffer::new(AdType::Uri);
+        let scheme: UriScheme = ProvisionedUriScheme::Http.into();
+        scheme.try_encode(&mut buffer)?;
+        assert_eq!(buffer.data(), &[0x03, 0x24, 0x16, 0x00]);
+
+        let mut buffer = AdStructBuffer::new(AdType::Uri);
+        let scheme: UriScheme = custom_uri_scheme!("custom").into();
+        scheme.try_encode(&mut buffer)?;
+        assert_eq!(
+            buffer.data(),
+            &[0x0A, 0x24, 0x01, 0x00, b'c', b'u', b's', b't', b'o', b'm', b':']
+        );
 
         Ok(())
     }
 
     #[test]
-    fn test_custom_uri_scheme_creation_failed() {
-        let err = CustomUriScheme::try_new("").expect_err("Invalid custom URI scheme");
-        assert!(matches!(err, AdvertisingError::InvalidCustomUriScheme("")));
-
-        let err = CustomUriScheme::try_new("2bedefined").expect_err("Invalid custom URI scheme");
+    fn test_uri_scheme_try_encode_failure() {
+        let mut buffer = AdStructBuffer::new(AdType::Uri);
+        let scheme: UriScheme = custom_uri_scheme!("very-very-very-long-custom-scheme").into();
+        let err = scheme
+            .try_encode(&mut buffer)
+            .expect_err("Custom URI scheme too long");
         assert!(matches!(
             err,
-            AdvertisingError::InvalidCustomUriScheme("2bedefined")
+            AdvertisingError::AdvertisingDataWillNotFitAdvertisingPacket
+        ));
+    }
+
+    #[test]
+    fn test_uri_try_encode_success() -> Result<(), AdvertisingError> {
+        let mut buffer = AdStructBuffer::new(AdType::Uri);
+        let uri: Uri = Uri::new(ProvisionedUriScheme::Http, "//example.org/");
+        uri.try_encode(&mut buffer)?;
+        assert_eq!(
+            buffer.data(),
+            &[
+                0x11, 0x24, 0x16, 0x00, b'/', b'/', b'e', b'x', b'a', b'm', b'p', b'l', b'e', b'.',
+                b'o', b'r', b'g', b'/'
+            ]
+        );
+
+        let mut buffer = AdStructBuffer::new(AdType::Uri);
+        let uri: Uri = Uri::new(custom_uri_scheme!("custom"), "rest");
+        uri.try_encode(&mut buffer)?;
+        assert_eq!(
+            buffer.data(),
+            &[
+                0x0E, 0x24, 0x01, 0x00, b'c', b'u', b's', b't', b'o', b'm', b':', b'r', b'e', b's',
+                b't'
+            ]
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_uri_try_encode_failure() {
+        let mut buffer = AdStructBuffer::new(AdType::Uri);
+        let uri: Uri = Uri::new(
+            ProvisionedUriScheme::Http,
+            "//example.org/a-path-that-is-too-long",
+        );
+        let err = uri.try_encode(&mut buffer).expect_err("URI too long");
+        assert!(matches!(
+            err,
+            AdvertisingError::AdvertisingDataWillNotFitAdvertisingPacket
         ));
 
-        let err = CustomUriScheme::try_new("-mine").expect_err("Invalid custom URI scheme");
+        let mut buffer = AdStructBuffer::new(AdType::Uri);
+        let uri: Uri = Uri::new(custom_uri_scheme!("custom"), "a-hier-part-that-is-too-long");
+        let err = uri.try_encode(&mut buffer).expect_err("URI too long");
         assert!(matches!(
             err,
-            AdvertisingError::InvalidCustomUriScheme("-mine")
+            AdvertisingError::AdvertisingDataWillNotFitAdvertisingPacket
+        ));
+    }
+
+    #[test]
+    fn test_uri_ad_struct_creation_success() -> Result<(), Error> {
+        let value = UriAdStruct::try_new(Uri::new(ProvisionedUriScheme::Http, "//example.org/"))?;
+        assert_eq!(
+            value.encoded_data(),
+            &[
+                0x11, 0x24, 0x16, 0x00, b'/', b'/', b'e', b'x', b'a', b'm', b'p', b'l', b'e', b'.',
+                b'o', b'r', b'g', b'/'
+            ]
+        );
+        assert!(value.r#type().contains(AdStructType::URI));
+        assert!(!value.is_unique());
+
+        let value = UriAdStruct::try_new(Uri::new(custom_uri_scheme!("custom"), "uri"))?;
+        assert_eq!(
+            value.encoded_data(),
+            &[0x0D, 0x24, 0x01, 0x00, b'c', b'u', b's', b't', b'o', b'm', b':', b'u', b'r', b'i']
+        );
+        assert!(value.r#type().contains(AdStructType::URI));
+        assert!(!value.is_unique());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_uri_ad_struct_creation_failure() {
+        let err = UriAdStruct::try_new(Uri::new(
+            ProvisionedUriScheme::Http,
+            "//example.org/a-path-that-is-too-long",
+        ))
+        .expect_err("Uri does not fit");
+        assert!(matches!(
+            err,
+            Error::Advertising(AdvertisingError::AdvertisingDataWillNotFitAdvertisingPacket)
         ));
 
-        let err = CustomUriScheme::try_new(".yours").expect_err("Invalid custom URI scheme");
+        let err = UriAdStruct::try_new(Uri::new(
+            custom_uri_scheme!("very-very-very-long-custom-scheme"),
+            "rest",
+        ))
+        .expect_err("Uri does not fit");
         assert!(matches!(
             err,
-            AdvertisingError::InvalidCustomUriScheme(".yours")
-        ));
-
-        let err = CustomUriScheme::try_new("mine.").expect_err("Invalid custom URI scheme");
-        assert!(matches!(
-            err,
-            AdvertisingError::InvalidCustomUriScheme("mine.")
-        ));
-
-        let err = CustomUriScheme::try_new("yours-").expect_err("Invalid custom URI scheme");
-        assert!(matches!(
-            err,
-            AdvertisingError::InvalidCustomUriScheme("yours-")
-        ));
-
-        let err = CustomUriScheme::try_new("scheme:").expect_err("Invalid custom URI scheme");
-        assert!(matches!(
-            err,
-            AdvertisingError::InvalidCustomUriScheme("scheme:")
+            Error::Advertising(AdvertisingError::AdvertisingDataWillNotFitAdvertisingPacket)
         ));
     }
 }
