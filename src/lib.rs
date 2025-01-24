1a8af64955207a77b6d339112834759b775c1adf
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2025-01-24 23:36:45 +0100

    refactor: Use state machine for BLE host

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 use bitflags::Flags;
 use core::cell::{BorrowMutError, RefCell};
+use core::marker::PhantomData;
 use embedded_io::Error as EmbeddedIoError;
 
 pub mod advertising;
@@ -84,112 +85,23 @@ impl Default for ControllerCapabilities {
     }
 }
 
-pub struct BleDevice<T>
+#[derive(Debug)]
+struct Hci<HCID>
 where
-    T: embedded_io::Read + embedded_io::Write,
+    HCID: embedded_io::Read + embedded_io::Write,
 {
-    hci: RefCell<T>,
-    controller_capabilities: ControllerCapabilities,
+    driver: RefCell<HCID>,
 }
 
-impl<T> BleDevice<T>
+impl<HCID> Hci<HCID>
 where
-    T: embedded_io::Read + embedded_io::Write,
-    <T as embedded_io::ErrorType>::Error: embedded_io::Error,
+    HCID: embedded_io::Read + embedded_io::Write,
+    <HCID as embedded_io::ErrorType>::Error: embedded_io::Error,
 {
-    pub fn new(hci: T) -> Self {
+    fn new(hci_driver: HCID) -> Self {
         Self {
-            hci: hci.into(),
-            controller_capabilities: Default::default(),
+            driver: RefCell::new(hci_driver),
         }
-    }
-
-    // Perform setup has described in Core specification 4.2, Vol. 6, Part D, 2.1
-    pub fn setup(&mut self) -> Result<(), Error> {
-        self.cmd_reset()?;
-
-        self.cmd_read_local_supported_commands()?;
-        self.cmd_read_local_supported_features()?;
-        if !self
-            .controller_capabilities
-            .supported_features
-            .contains(SupportedFeatures::LE_SUPPORTED_CONTROLLER)
-        {
-            return Err(Error::NonLeCapableController);
-        }
-        self.set_event_mask()?;
-        // TODO: set LE event mask
-        self.cmd_le_read_buffer_size()?;
-        if (self.controller_capabilities.le_data_packet_length == 0)
-            || (self.controller_capabilities.num_le_data_packets == 0)
-                && self
-                    .controller_capabilities
-                    .supported_commands
-                    .contains(SupportedCommands::READ_BUFFER_SIZE)
-        {
-            self.cmd_read_buffer_size()?;
-        }
-        if self
-            .controller_capabilities
-            .supported_commands
-            .contains(SupportedCommands::LE_READ_LOCAL_SUPPORTED_FEATURES_PAGE_0)
-        {
-            self.cmd_le_read_local_supported_features_page_0()?;
-        }
-        self.cmd_le_read_supported_states()?; // TODO: needed??
-
-        Ok(())
-    }
-
-    pub fn supported_commands(&self) -> &SupportedCommands {
-        &self.controller_capabilities.supported_commands
-    }
-
-    pub fn supported_features(&self) -> &SupportedFeatures {
-        &self.controller_capabilities.supported_features
-    }
-
-    pub fn supported_le_features(&self) -> &SupportedLeFeatures {
-        &self.controller_capabilities.supported_le_features
-    }
-
-    pub fn supported_le_states(&self) -> &SupportedLeStates {
-        &self.controller_capabilities.supported_le_states
-    }
-
-    pub fn start_advertising(
-        &self,
-        adv_params: &AdvertisingParameters,
-        adv_data: &AdvertisingData,
-        scanresp_data: Option<&ScanResponseData>,
-    ) -> Result<(), Error> {
-        // TODO: Check state
-        self.cmd_le_set_advertising_parameters(adv_params)?;
-        // TODO: Read Advertising Channel Tx Power
-        self.cmd_le_set_advertising_data(adv_data)?;
-        let default_scanresp_data = ScanResponseData::default();
-        self.cmd_le_set_scan_response_data(match scanresp_data {
-            Some(scanresp_data) => scanresp_data,
-            None => &default_scanresp_data,
-        })?;
-        self.cmd_le_set_advertise_enable(AdvertisingEnable::Enabled)?;
-        Ok(())
-    }
-
-    pub fn stop_advertising(&self) -> Result<(), Error> {
-        self.cmd_le_set_advertise_enable(AdvertisingEnable::Disabled)?;
-        Ok(())
-    }
-
-    fn set_event_mask(&self) -> Result<CommandCompleteEvent, Error> {
-        let event_mask = EventMask::HARDWARE_ERROR
-            | EventMask::DATA_BUFFER_OVERFLOW
-            | EventMask::LE_META
-            | EventMask::DISCONNECTION_COMPLETE
-            | EventMask::READ_REMOTE_VERSION_INFORMATION_COMLETE
-            | EventMask::ENCRYPTION_CHANGE
-            | EventMask::ENCRYPTION_KEY_REFRESH_COMPLETE;
-        self.cmd_set_event_mask(event_mask)
     }
 
     fn execute_command(&self, command: Command) -> Result<CommandCompleteEvent, Error> {
@@ -210,61 +122,53 @@ where
     }
 
     fn cmd_reset(&mut self) -> Result<CommandCompleteEvent, Error> {
-        let event = self.execute_command(Command::Reset)?;
-        self.controller_capabilities.num_hci_command_packets =
-            event.num_hci_command_packets as usize;
-        Ok(event)
+        self.execute_command(Command::Reset)
     }
 
-    fn cmd_read_local_supported_commands(&mut self) -> Result<CommandCompleteEvent, Error> {
+    fn cmd_read_local_supported_commands(
+        &mut self,
+    ) -> Result<SupportedCommandsEventParameter, Error> {
         let event = self.execute_command(Command::ReadLocalSupportedCommands)?;
         let supported_commands_event_parameter: SupportedCommandsEventParameter =
             event.return_parameters.slice(1)?[..64].try_into()?;
-        self.controller_capabilities.supported_commands = supported_commands_event_parameter.value;
-        Ok(event)
+        Ok(supported_commands_event_parameter)
     }
 
-    fn cmd_read_local_supported_features(&mut self) -> Result<CommandCompleteEvent, Error> {
+    fn cmd_read_local_supported_features(&mut self) -> Result<LmpFeaturesEventParameter, Error> {
         let event = self.execute_command(Command::ReadLocalSupportedFeatures)?;
         let lmp_features_event_parameter: LmpFeaturesEventParameter =
             event.return_parameters.le_u64(1)?.into();
-        self.controller_capabilities.supported_features = lmp_features_event_parameter.value;
-        Ok(event)
+        Ok(lmp_features_event_parameter)
     }
 
-    fn cmd_read_buffer_size(&mut self) -> Result<CommandCompleteEvent, Error> {
+    fn cmd_read_buffer_size(&mut self) -> Result<(usize, usize), Error> {
         let event = self.execute_command(Command::ReadBufferSize)?;
-        self.controller_capabilities.le_data_packet_length =
-            event.return_parameters.le_u16(1)? as usize;
-        self.controller_capabilities.num_le_data_packets =
-            event.return_parameters.le_u16(4)? as usize;
-        Ok(event)
+        let le_data_packet_length = event.return_parameters.le_u16(1)? as usize;
+        let num_le_data_packets = event.return_parameters.le_u16(4)? as usize;
+        Ok((le_data_packet_length, num_le_data_packets))
     }
 
     fn cmd_le_read_local_supported_features_page_0(
         &mut self,
-    ) -> Result<CommandCompleteEvent, Error> {
+    ) -> Result<LeFeaturesEventParameter, Error> {
         let event = self.execute_command(Command::LeReadLocalSupportedFeaturesPage0)?;
         let le_features_event_parameter: LeFeaturesEventParameter =
             event.return_parameters.le_u64(1)?.into();
-        self.controller_capabilities.supported_le_features = le_features_event_parameter.value;
-        Ok(event)
+        Ok(le_features_event_parameter)
     }
 
-    fn cmd_le_read_buffer_size(&mut self) -> Result<CommandCompleteEvent, Error> {
+    fn cmd_le_read_buffer_size(&mut self) -> Result<(usize, usize), Error> {
         let event = self.execute_command(Command::LeReadBufferSize)?;
-        self.controller_capabilities.le_data_packet_length =
-            event.return_parameters.le_u16(1)? as usize;
-        self.controller_capabilities.num_le_data_packets = event.return_parameters.u8(3)? as usize;
-        Ok(event)
+        let le_data_packet_length = event.return_parameters.le_u16(1)? as usize;
+        let num_le_data_packets = event.return_parameters.u8(3)? as usize;
+        Ok((le_data_packet_length, num_le_data_packets))
     }
 
-    fn cmd_le_read_supported_states(&mut self) -> Result<CommandCompleteEvent, Error> {
+    fn cmd_le_read_supported_states(&mut self) -> Result<LeStatesEventParameter, Error> {
         let event = self.execute_command(Command::LeReadSupportedStates)?;
         let le_states_event_parameter: LeStatesEventParameter =
             event.return_parameters.le_u64(1)?.into();
-        self.controller_capabilities.supported_le_states = le_states_event_parameter.value;
-        Ok(event)
+        Ok(le_states_event_parameter)
     }
 
     fn cmd_le_set_advertise_enable(
@@ -296,7 +200,7 @@ where
     }
 
     fn hci_write(&self, data: &[u8]) -> Result<usize, Error> {
-        self.hci
+        self.driver
             .try_borrow_mut()?
             .write(data)
             .map_err(|err| Error::IO(err.kind()))
@@ -335,7 +239,7 @@ where
                     )))
                 }
                 PacketType::Event => {
-                    let event = Event::read(&self.hci)?;
+                    let event = Event::read(&self.driver)?;
                     return Ok(Some(HciPollResult::Event(event)));
                 }
                 PacketType::IsoData => {
@@ -351,11 +255,188 @@ where
 
     fn hci_read(&self) -> Result<Option<u8>, Error> {
         let mut buffer = [0u8];
-        let res = self.hci.try_borrow_mut()?.read(&mut buffer);
+        let res = self.driver.try_borrow_mut()?.read(&mut buffer);
         match res {
             Ok(1) => Ok(Some(buffer[0])),
             _ => Ok(None),
         }
+    }
+}
+
+pub trait HostState {}
+
+pub struct BleHost<'a, HCID, State: HostState = Initial>
+where
+    HCID: embedded_io::Read + embedded_io::Write,
+{
+    controller_capabilities: ControllerCapabilities,
+    hci: &'a Hci<HCID>,
+    phantom: PhantomData<State>,
+}
+
+#[derive(Debug, Default)]
+pub struct Initial;
+#[derive(Debug, Default)]
+pub struct Standby;
+#[derive(Debug, Default)]
+pub struct Advertising;
+
+impl HostState for Initial {}
+impl HostState for Standby {}
+impl HostState for Advertising {}
+
+impl<'a, HCID> BleHost<'a, HCID, Initial>
+where
+    HCID: embedded_io::Read + embedded_io::Write,
+{
+    // Perform setup has described in Core specification 4.2, Vol. 6, Part D, 2.1
+    fn setup(hci: &'a mut Hci<HCID>) -> Result<BleHost<'a, HCID, Standby>, Error>
+    where
+        HCID: embedded_io::Read + embedded_io::Write,
+    {
+        let mut controller_capabilities = ControllerCapabilities::default();
+
+        hci.cmd_reset()?;
+
+        controller_capabilities.supported_commands = hci.cmd_read_local_supported_commands()?.value;
+        controller_capabilities.supported_features = hci.cmd_read_local_supported_features()?.value;
+        if !controller_capabilities
+            .supported_features
+            .contains(SupportedFeatures::LE_SUPPORTED_CONTROLLER)
+        {
+            return Err(Error::NonLeCapableController);
+        }
+
+        let event_mask = EventMask::HARDWARE_ERROR
+            | EventMask::DATA_BUFFER_OVERFLOW
+            | EventMask::LE_META
+            | EventMask::DISCONNECTION_COMPLETE
+            | EventMask::READ_REMOTE_VERSION_INFORMATION_COMLETE
+            | EventMask::ENCRYPTION_CHANGE
+            | EventMask::ENCRYPTION_KEY_REFRESH_COMPLETE;
+        hci.cmd_set_event_mask(event_mask)?;
+        // // TODO: set LE event mask
+
+        (
+            controller_capabilities.le_data_packet_length,
+            controller_capabilities.num_le_data_packets,
+        ) = hci.cmd_le_read_buffer_size()?;
+        if (controller_capabilities.le_data_packet_length == 0)
+            || (controller_capabilities.num_le_data_packets == 0)
+                && controller_capabilities
+                    .supported_commands
+                    .contains(SupportedCommands::READ_BUFFER_SIZE)
+        {
+            (
+                controller_capabilities.le_data_packet_length,
+                controller_capabilities.num_le_data_packets,
+            ) = hci.cmd_read_buffer_size()?;
+        }
+        if controller_capabilities
+            .supported_commands
+            .contains(SupportedCommands::LE_READ_LOCAL_SUPPORTED_FEATURES_PAGE_0)
+        {
+            controller_capabilities.supported_le_features =
+                hci.cmd_le_read_local_supported_features_page_0()?.value;
+        }
+        controller_capabilities.supported_le_states = hci.cmd_le_read_supported_states()?.value; // TODO: needed??
+
+        Ok(BleHost::<HCID, Standby> {
+            controller_capabilities,
+            hci,
+            phantom: PhantomData,
+        })
+    }
+}
+
+impl<'a, HCID> BleHost<'a, HCID, Standby>
+where
+    HCID: embedded_io::Read + embedded_io::Write,
+{
+    pub fn start_advertising(
+        self,
+        adv_params: &AdvertisingParameters,
+        adv_data: &AdvertisingData,
+        scanresp_data: Option<&ScanResponseData>,
+    ) -> Result<BleHost<'a, HCID, Advertising>, Error> {
+        self.hci.cmd_le_set_advertising_parameters(adv_params)?;
+        // TODO: Read Advertising Channel Tx Power
+        self.hci.cmd_le_set_advertising_data(adv_data)?;
+        let default_scanresp_data = ScanResponseData::default();
+        self.hci
+            .cmd_le_set_scan_response_data(match scanresp_data {
+                Some(scanresp_data) => scanresp_data,
+                None => &default_scanresp_data,
+            })?;
+        self.hci
+            .cmd_le_set_advertise_enable(AdvertisingEnable::Enabled)?;
+        Ok(BleHost::<HCID, Advertising> {
+            controller_capabilities: self.controller_capabilities,
+            hci: self.hci,
+            phantom: PhantomData,
+        })
+    }
+}
+
+impl<'a, HCID> BleHost<'a, HCID, Advertising>
+where
+    HCID: embedded_io::Read + embedded_io::Write,
+{
+    pub fn stop_advertising(self) -> Result<BleHost<'a, HCID, Standby>, Error> {
+        self.hci
+            .cmd_le_set_advertise_enable(AdvertisingEnable::Disabled)?;
+        Ok(BleHost::<HCID, Standby> {
+            controller_capabilities: self.controller_capabilities,
+            hci: self.hci,
+            phantom: PhantomData,
+        })
+    }
+}
+
+impl<'a, HCID> BleHost<'a, HCID>
+where
+    HCID: embedded_io::Read + embedded_io::Write,
+{
+    pub fn supported_commands(&self) -> &SupportedCommands {
+        &self.controller_capabilities.supported_commands
+    }
+
+    pub fn supported_features(&self) -> &SupportedFeatures {
+        &self.controller_capabilities.supported_features
+    }
+
+    pub fn supported_le_features(&self) -> &SupportedLeFeatures {
+        &self.controller_capabilities.supported_le_features
+    }
+
+    pub fn supported_le_states(&self) -> &SupportedLeStates {
+        &self.controller_capabilities.supported_le_states
+    }
+}
+
+pub struct BleDevice<HCID>
+where
+    HCID: embedded_io::Read + embedded_io::Write,
+{
+    hci: Hci<HCID>,
+}
+
+impl<HCID> BleDevice<HCID>
+where
+    HCID: embedded_io::Read + embedded_io::Write,
+{
+    pub fn new(hci_driver: HCID) -> Self {
+        Self {
+            hci: Hci::new(hci_driver),
+        }
+    }
+
+    pub fn run(&mut self) -> Result<(), Error> {
+        let host = BleHost::setup(&mut self.hci)?;
+
+        todo!();
+
+        Ok(())
     }
 }
 
